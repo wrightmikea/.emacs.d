3ebbdc8155f9a1bcb7fb6795463921e9ce46d2a7
Andreas Gerler <baron@bundesbrandschatzamt.de>
2016-08-11 20:38:53 +0200

    inital commit for minimal branch.

--- elpa/rainbow-mode-0.12/rainbow-mode-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; rainbow-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "rainbow-mode" "rainbow-mode.el" (22216 26216
-;;;;;;  0 0))
-;;; Generated autoloads from rainbow-mode.el
-
-(autoload 'rainbow-mode "rainbow-mode" "\
-Colorize strings that represent colors.
-This will fontify with colors the string like \"#aabbcc\" or \"blue\".
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("rainbow-mode-pkg.el") (22216 26216 901245
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; rainbow-mode-autoloads.el ends here
